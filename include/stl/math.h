6c9b19a4341a1a17dbd07d0fca13ea25b945d114
Amber Brault <celestialamber1@gmail.com>
2023-02-17 21:35:29 -0500

    derp

---
+++ include/stl/math.h
@@ -0,0 +1,21 @@
+#ifndef _MATH_H
+#define _MATH_H
+
+#include "types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif // ifdef __cplusplus
+
+#define PI 3.14159265
+
+
+float sqrtf(float);
+
+
+
+#ifdef __cplusplus
+};
+#endif // ifdef __cplusplus
+
+#endif
