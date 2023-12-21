b76d8b6b19665a37b2d2c6de5586375cb705731a
Amber Brault <celestialamber1@gmail.com>
2023-12-21 18:06:07 -0500

    I am very dumb and deserve a million bonks

--- tools/download_mwcc.py
+++ tools/download_mwcc.py
@@ -7,36 +7,38 @@ import shutil
 import zipfile
 
 if sys.platform == "cygwin":
-    sys.exit(
-        f"Cygwin/MSYS2 is not supported."
-        f"\nPlease use native Windows Python instead."
-        f"\nPlease run pacman -R python in msys2."
-        f"\n(Current path: {sys.executable})"
-    )
+	sys.exit(
+		f"Cygwin/MSYS2 is not supported."
+		f"\nPlease use native Windows Python instead."
+		f"\nPlease run pacman -R python in msys2."
+		f"\n(Current path: {sys.executable})"
+	)
 
 HARDLINK = "https://files.decomp.dev/compilers_20230715.zip"
 
 
 def main() -> None:
-    output = f"{os.path.dirname(__file__)}/mwcc_compiler"
+	output = f"{os.path.dirname(__file__)}/mwcc_compiler"
 
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        tmp_zip = f"{tmp_dir}/mwcc_compiler.zip"
+	with tempfile.TemporaryDirectory() as tmp_dir:
+		tmp_zip = f"{tmp_dir}/mwcc_compiler.zip"
 
-        request = urllib.request.Request(
-            url=HARDLINK,
-            headers={"User-Agent": "Mozilla/5.0"},
-        )
+		request = urllib.request.Request(
+			url=HARDLINK,
+			headers={"User-Agent": "Mozilla/5.0"},
+		)
 
-        with urllib.request.urlopen(request) as src, open(tmp_zip, "wb") as dst:
-            shutil.copyfileobj(src, dst)
+		with urllib.request.urlopen(request) as src, open(tmp_zip, "wb") as dst:
+			shutil.copyfileobj(src, dst)
 
-        with zipfile.ZipFile(tmp_zip) as zip_file:
-            zip_file.extractall(tmp_dir)
+		with zipfile.ZipFile(tmp_zip) as zip_file:
+			zip_file.extractall(tmp_dir)
 
-    st = os.stat(output)
-    os.chmod(output, st.st_mode | stat.S_IEXEC)
+		shutil.move(tmp_dir, output)
+
+	st = os.stat(output)
+	os.chmod(output, st.st_mode | stat.S_IEXEC)
 
 
 if __name__ == "__main__":
-    main()
+	main()
