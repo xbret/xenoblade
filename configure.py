873814d1563d3c4284573b3fc8a2d20ad2afdb51
Amber Brault <celestialamber1@gmail.com>
2024-08-10 17:39:06 -0400

    Porting part 6: split data for libs into temp files

--- configure.py
+++ configure.py
@@ -319,7 +319,7 @@ Matching = True                   # Object matches and should be linked
 NonMatching = False               # Object does not match and should not be linked
 Equivalent = config.non_matching  # Object should be linked when configured with --non-matching
 
-config.warn_missing_config = False
+config.warn_missing_config = True
 config.warn_missing_source = False
 config.libs = [
     {
