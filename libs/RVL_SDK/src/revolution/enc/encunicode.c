bb0053251d0a57519d40487972812f7e6d5c5c37
Amber Brault <celestialamber1@gmail.com>
2024-09-09 16:34:09 -0400

    Cleanup enc stuffs

--- libs/RVL_SDK/src/revolution/enc/encunicode.c
+++ libs/RVL_SDK/src/revolution/enc/encunicode.c
@@ -1,11 +1,130 @@
 #include <revolution/enc/encunicode.h>
 
+//unused
+static char base64_array[] = {
+    0
+};
+
 ENCResult ENCiConvertStringUtf8ToUtf16(UNKWORD r3, UNKWORD r4, UNKWORD r5, UNKWORD r6, UNKWORD r7);
+void ENCiConvertUtf8To32();
+void ENCiConvertUtf32To16();
+
+//unused
+void ENCConvertStringUnicodeToAscii(){
+}
+
+//unused
+void ENCConvertStringAsciiToUnicode(){
+}
+
+//unused
+void ENCConvertStringUtf32ToUtf16(){
+}
+
+//unused
+void ENCConvertStringUtf16ToUtf32(){
+}
+
+//unused
+void ENCConvertStringUtf32ToUtf8(){
+}
+
+//unused
+void ENCConvertStringUtf8ToUtf32(){
+}
+
+//unused
+void ENCConvertStringUtf16ToUtf8(){
+}
 
 ENCResult ENCConvertStringUtf8ToUtf16(UNKWORD r3, UNKWORD r4, UNKWORD r5, UNKWORD r6){
     return ENCiConvertStringUtf8ToUtf16(r3,r4,r5,r6,0);
 }
 
+
+//unused
+void ENCConvertStringUtf16ToUtf16(){
+}
+
+//unused
+void ENCConvertStringUtf16LEToUtf16BE(){
+}
+
+//unused
+void ENCConvertStringUtf7ToUtf16(){
+}
+
+//unused
+void ENCSetUnicodeBOM(){
+}
+
+//unused
+void ENCSetUnicodeBOM32(){
+}
+
+//unused
+void ENCSetUnicodeBOM16(){
+}
+
+//unused
+void ENCSetUnicodeBOM8(){
+}
+
+//unused
+void ENCiConvertStringUnicodeToAscii(){
+}
+
+//unused
+void ENCiConvertStringAsciiToUnicode(){
+}
+
+//unused
+void ENCiConvertStringUtf32ToUtf16(){
+}
+
+//unused
+void ENCiConvertStringUtf16ToUtf32(){
+}
+
+//unused
+void ENCiConvertStringUtf32ToUtf8(){
+}
+
+//unused
+void ENCiConvertStringUtf8ToUtf32(){
+}
+
+//unused
+void ENCiConvertStringUtf16ToUtf8(){
+}
+
 ENCResult ENCiConvertStringUtf8ToUtf16(UNKWORD r3, UNKWORD r4, UNKWORD r5, UNKWORD r6, UNKWORD r7){
 
 }
+
+//unused
+void ENCiConvertStringUtf7ToUtf16(){
+}
+
+//unused
+void ENCiConvertStringUtf16ToUtf16(){
+}
+
+static void ENCiConvertUtf8To32(){
+}
+
+//unused
+static void ENCiConvertUtf32To8(){
+}
+
+//unused
+static void ENCiConvertUtf16To32(){
+}
+
+static void ENCiConvertUtf32To16(){
+}
+
+//unused
+static void ENCiGetBase64Value(){
+}
+
