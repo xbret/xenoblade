ee39e19a06b35bc1c43ea50cec14314ed4cee1e6
Amber Brault <celestialamber1@gmail.com>
2025-11-27 08:57:56 +0100

    Update misc stuff

--- libs/monolib/include/monolib/work/CWorkThread.hpp
+++ libs/monolib/include/monolib/work/CWorkThread.hpp
@@ -5,7 +5,6 @@
 #include "monolib/CMsgParam.hpp"
 #include "monolib/FixStr.hpp"
 #include "monolib/IWorkEvent.hpp"
-#include "monolib/Math.hpp"
 #include "monolib/MemManager.hpp"
 #include "monolib/reslist.hpp"
 #include "monolib/work/CWorkThreadSystem.hpp"
