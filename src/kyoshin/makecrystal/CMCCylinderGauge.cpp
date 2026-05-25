fe7f933282c584141f34a2e06e334638ae101264
Noaseto <noix.50@hotmail.fr>
2026-05-25 21:48:13 +0200

    makecrystal/CMCCylinderGauge.cpp OK

--- src/kyoshin/makecrystal/CMCCylinderGauge.cpp
+++ src/kyoshin/makecrystal/CMCCylinderGauge.cpp
@@ -1,219 +1,225 @@
 #include "kyoshin/makecrystal/CMCCylinderGauge.hpp"
 
+#include "kyoshin/code_80135FDC.hpp"
+#include "monolib/math/MTRand.hpp"
+
 extern void func_80136F08(nw4r::lyt::Layout*, nw4r::lyt::AnimTransform**, nw4r::lyt::ArcResourceAccessor*, char*);
 extern u32 func_801355A0();
 extern void func_801368C0(nw4r::lyt::Layout*, char*, u32);
 extern void func_80136910(nw4r::lyt::Layout*, char*, u8);
-extern u32 func_80137444(nw4r::lyt::AnimTransform*, f32);
+extern u32 func_80137444(nw4r::lyt::AnimTransform*, float);
 extern void func_80138078(u32);
 extern void func_8013B428(u32);
 
-CMCCylinderGauge::CMCCylinderGauge(nw4r::lyt::ArcResourceAccessor* arg){
-    pArcResourceAccessor = arg;
+CMCCylinderGauge::CMCCylinderGauge(nw4r::lyt::ArcResourceAccessor* arg) {
+    mArcResourceAccessor = arg;
     mLayout = nullptr;
-    pAnimTrans_0x0C = nullptr;
-    pAnimTrans_0x10 = nullptr;
-    pAnimTrans_0x14 = nullptr;
-    pAnimTrans_0x18 = nullptr;
-    bool_0x1c = false;
-    unk_0x20 = 0;
-    bool_0x24 = true;
-    unk_0x25 = 1;
-    float_0x28 = 0;
+    mAnimTransC = nullptr;
+    mAnimTrans10 = nullptr;
+    mAnimTrans14 = nullptr;
+    mAnimTrans18 = nullptr;
+    unk1c = false;
+    unk20 = 0;
+    unk24 = true;
+    unk25 = 1;
+    unk28 = 0.0f;
 }
 
-CMCCylinderGauge::~CMCCylinderGauge(){
-}
+CMCCylinderGauge::~CMCCylinderGauge() {}
 
-void CMCCylinderGauge::func_80221EF4(){
-    func_80136E84(&mLayout, pArcResourceAccessor, "mf10_cry01_gage.brlyt");
+void CMCCylinderGauge::func_80221EF4() {
+    func_80136E84(&mLayout, mArcResourceAccessor, "mf10_cry01_gage.brlyt");
 
-    func_80136F08(mLayout, &pAnimTrans_0x0C, pArcResourceAccessor, "mf10_cry01_gage_in.brlan");
-    func_80136F08(mLayout, &pAnimTrans_0x10, pArcResourceAccessor, "mf10_cry01_gage_up.brlan");
-    func_80136F08(mLayout, &pAnimTrans_0x14, pArcResourceAccessor, "mf10_cry01_gage_full.brlan");
-    func_80136F08(mLayout, &pAnimTrans_0x18, pArcResourceAccessor, "mf10_cry01_gage_out.brlan");
-    
-    func_801368C0(mLayout, "txt_sylinder",func_801355A0());
-    func_80136910(mLayout, "txt_sylinder", unk_0x25);
-    
-    float_0x28=0.0f;
-    this->func_80222520();
+    func_80136F08(mLayout, &mAnimTransC, mArcResourceAccessor, "mf10_cry01_gage_in.brlan");
+    func_80136F08(mLayout, &mAnimTrans10, mArcResourceAccessor, "mf10_cry01_gage_up.brlan");
+    func_80136F08(mLayout, &mAnimTrans14, mArcResourceAccessor, "mf10_cry01_gage_full.brlan");
+    func_80136F08(mLayout, &mAnimTrans18, mArcResourceAccessor, "mf10_cry01_gage_out.brlan");
+
+    func_801368C0(mLayout, "txt_sylinder", func_801355A0());
+    func_80136910(mLayout, "txt_sylinder", unk25);
+
+    unk28 = 0.0f;
+    func_80222520();
     mLayout->Animate(0);
-    bool_0x1c = true;
+    unk1c = true;
 }
 
-void CMCCylinderGauge::func_80221FE0(){
-    if (bool_0x1c == 0){
+void CMCCylinderGauge::func_80221FE0() {
+    if(unk1c == 0) {
         return;
     }
-    switch ((s32)unk_0x20) {
-        case 1: 
-            this->func_802222C4(); 
+    switch((s32)unk20) {
+        case 1:
+            func_802222C4();
             break;
         case 2:
-            this->func_80222318();
+            func_80222318();
             break;
         case 3:
-            this->func_8022246C();
+            func_8022246C();
             break;
         case 4:
-            this->func_802224B0();
+            func_802224B0();
             break;
     }
     mLayout->Animate(0);
 }
 
-void CMCCylinderGauge::func_80222070(nw4r::lyt::DrawInfo* pDrawInfo){
-    if (bool_0x1c != 0){
+void CMCCylinderGauge::func_80222070(nw4r::lyt::DrawInfo* pDrawInfo) {
+    if(unk1c != 0) {
         func_80137038(mLayout, pDrawInfo, 0, 1);
     }
 }
 
-void CMCCylinderGauge::func_80222090(){
-    bool_0x1c = false;
-    if(mLayout != nullptr){
+void CMCCylinderGauge::func_80222090() {
+    unk1c = false;
+    if(mLayout != nullptr) {
         delete mLayout;
         mLayout = nullptr;
     }
 }
 
-bool CMCCylinderGauge::func_802220F0(){
-    return bool_0x24;
+bool CMCCylinderGauge::func_802220F0() {
+    return unk24;
 }
 
-void CMCCylinderGauge::func_802220F8(){
-    if(unk_0x20 == 0){
-        unk_0x20 = 1;
-        bool_0x24 = false;
+void CMCCylinderGauge::func_802220F8() {
+    if(unk20 == 0) {
+        unk20 = 1;
+        unk24 = false;
     }
 }
 
-void CMCCylinderGauge::func_80222118(){
-    unk_0x20 = 3;
-    bool_0x24 = false;
-    if (float_0x28 > 0.0f){
-        unk_0x25 = (u8)((f32)unk_0x25 + float_0x28);
-        if (unk_0x25 > 9){
-            unk_0x25 = 9;
+void CMCCylinderGauge::func_80222118() {
+    unk20 = 3;
+    unk24 = false;
+    if(unk28 > 0.0f) {
+        unk25 = (u8)((float)unk25 + unk28);
+        if(unk25 > 9) {
+            unk25 = 9;
         }
-        float_0x28 = 0.0f;
+        unk28 = 0.0f;
     }
-    this->func_802226B8();
+    func_802226B8();
 }
 
-void CMCCylinderGauge::func_802221A4(f32 arg){
-    // not fully matching yet, behaviour seems ok though.
-    // I struggle with assembly optimization
-    u32 random = ml::MTRand::getInstance()->rand31();
-    u32 div = random / 41;
-    u32 mod = random - (div * 41);
+//Optimization level is reduced to ensure the remainder is naively computed
+#pragma push
+#pragma optimization_level 1
+void CMCCylinderGauge::func_802221A4(float arg) {
+    s32 random = (s32)ml::MTRand::getInstance()->rand31();
 
-    f32 fVar = (f32)mod + 30.0f;
-    fVar += arg;
-    fVar *= 0.01f;
-    float_0x28 += fVar;
+    s32 divisor = 0x29;
+    s32 remainder = random % divisor;
+
+    float temp = (float)remainder;
+    temp += 30.0f;
+    temp += arg;
+    temp *= 0.01f;
+    unk28 += temp;
+}
+#pragma pop
+
+void CMCCylinderGauge::func_80222234() {
+    unk25 -= 1;
+    func_80136910(mLayout, "txt_sylinder", unk25);
 }
 
-void CMCCylinderGauge::func_80222234(){
-    unk_0x25 -= 1;
-    func_80136910(mLayout, "txt_sylinder", unk_0x25);
-}
-
-u8 CMCCylinderGauge::func_80222258(){
-    u8 result = (u8)(unk_0x25 + float_0x28);
-    if (result > 9){
+u8 CMCCylinderGauge::func_80222258() {
+    u8 result = (u8)(unk25 + unk28);
+    if(result > 9) {
         result = 9;
     }
     return result;
 }
 
-void CMCCylinderGauge::func_802222A4(u8 arg){
-    unk_0x25 = arg;
-    func_80136910(mLayout, "txt_sylinder", unk_0x25);
+void CMCCylinderGauge::func_802222A4(u8 arg) {
+    unk25 = arg;
+    func_80136910(mLayout, "txt_sylinder", unk25);
 }
 
-void CMCCylinderGauge::func_802222C4(){
-    if(func_80137444(pAnimTrans_0x0C, 1.0f) == 0){
+void CMCCylinderGauge::func_802222C4() {
+    if(func_80137444(mAnimTransC, 1.0f) == 0) {
         return;
     }
-    bool_0x24 = true;
-    unk_0x20 = 2;
+    unk24 = true;
+    unk20 = 2;
     func_802225A8();
 }
 
-void CMCCylinderGauge::func_80222318(){
-    f32 fVar1 = float_0x28;
-    if (fVar1 > 1.0f){
+void CMCCylinderGauge::func_80222318() {
+    float fVar1 = unk28;
+    if(fVar1 > 1.0f) {
         fVar1 = 1.0f;
     }
-    f32 fVar2 = 1.0f + pAnimTrans_0x10->GetFrame();
-    u16 frameSize = pAnimTrans_0x10->GetFrameSize();
-    f32 fVar3 = fVar1 * ((f32)frameSize - 1.0f);
-    if(fVar2 > fVar3){
+    float fVar2 = 1.0f + mAnimTrans10->GetFrame();
+    u16 frameSize = mAnimTrans10->GetFrameSize();
+    float fVar3 = fVar1 * ((float)frameSize - 1.0f);
+    if(fVar2 > fVar3) {
         fVar2 = fVar3;
-        if(!(float_0x28 >= 1.0f)){
-            if(unk_0x25 >= 9){
-                unk_0x25++;
-                func_80136910(mLayout, "txt_sylinder", unk_0x25);
-                float_0x28--;
-                if(float_0x28<0.0f){
-                    float_0x28=0.0f;
+        if(unk28 >= 1.0f) {
+            if(unk25 < 9) {
+                unk25++;
+                func_80136910(mLayout, "txt_sylinder", unk25);
+                unk28--;
+                if(unk28 < 0.0f) {
+                    unk28 = 0.0f;
                 }
-                unk_0x20=4;
-                this->func_80222630();
-                pAnimTrans_0x14->SetFrame(0.0f);
+                unk20 = 4;
+                func_80222630();
+                mAnimTrans14->SetFrame(0.0f);
                 func_80138078(0xb1);
-            } else{
-                float_0x28 = 1.0f;
+            } else {
+                unk28 = 1.0f;
                 func_8013B428(0x80);
             }
         }
     }
-    pAnimTrans_0x10->SetFrame(fVar2);
+    mAnimTrans10->SetFrame(fVar2);
     mLayout->Animate(0);
 }
 
-void CMCCylinderGauge::func_8022246C(){
-    if(func_80137444(pAnimTrans_0x18, 1.0f) == 0){
+void CMCCylinderGauge::func_8022246C() {
+    if(func_80137444(mAnimTrans18, 1.0f) == 0) {
         return;
     }
-    bool_0x24 = true;
+    unk24 = true;
 }
 
-void CMCCylinderGauge::func_802224B0(){
-    if(func_80137444(pAnimTrans_0x14, 1.0f) == 0){
+void CMCCylinderGauge::func_802224B0() {
+    if(func_80137444(mAnimTrans14, 1.0f) == 0) {
         return;
     }
-    unk_0x20 = 2;
-    this->func_802225A8();
-    pAnimTrans_0x10->SetFrame(0.0f);
+    unk20 = 2;
+    func_802225A8();
+    mAnimTrans10->SetFrame(0.0f);
     mLayout->Animate(0);
 }
 
-void CMCCylinderGauge::func_80222520(){
+void CMCCylinderGauge::func_80222520() {
     mLayout->UnbindAllAnimation();
-    mLayout->BindAnimation(pAnimTrans_0x0C);
-    mLayout->SetAnimationEnable(pAnimTrans_0x0C, true);
+    mLayout->BindAnimation(mAnimTransC);
+    mLayout->SetAnimationEnable(mAnimTransC, true);
     mLayout->Animate(0);
 }
 
-void CMCCylinderGauge::func_802225A8(){
+void CMCCylinderGauge::func_802225A8() {
     mLayout->UnbindAllAnimation();
-    mLayout->BindAnimation(pAnimTrans_0x10);
-    mLayout->SetAnimationEnable(pAnimTrans_0x10, true);
+    mLayout->BindAnimation(mAnimTrans10);
+    mLayout->SetAnimationEnable(mAnimTrans10, true);
     mLayout->Animate(0);
 }
 
-void CMCCylinderGauge::func_80222630(){
+void CMCCylinderGauge::func_80222630() {
     mLayout->UnbindAllAnimation();
-    mLayout->BindAnimation(pAnimTrans_0x14);
-    mLayout->SetAnimationEnable(pAnimTrans_0x14, true);
+    mLayout->BindAnimation(mAnimTrans14);
+    mLayout->SetAnimationEnable(mAnimTrans14, true);
     mLayout->Animate(0);
 }
 
-void CMCCylinderGauge::func_802226B8(){
+void CMCCylinderGauge::func_802226B8() {
     mLayout->UnbindAllAnimation();
-    mLayout->BindAnimation(pAnimTrans_0x18);
-    mLayout->SetAnimationEnable(pAnimTrans_0x18, true);
+    mLayout->BindAnimation(mAnimTrans18);
+    mLayout->SetAnimationEnable(mAnimTrans18, true);
     mLayout->Animate(0);
 }
--- src/kyoshin/makecrystal/CMCCylinderGauge.hpp
+++ src/kyoshin/makecrystal/CMCCylinderGauge.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <types.h>
-#include <nw4r/lyt/lyt_layout.h>
-#include <nw4r/lyt/lyt_drawInfo.h>
-#include <nw4r/lyt/lyt_resourceAccessor.h>
+
 #include <nw4r/lyt/lyt_animation.h>
-#include "monolib/math/MTRand.hpp"
-#include "kyoshin/code_80135FDC.hpp"
+#include <nw4r/lyt/lyt_drawInfo.h>
+#include <nw4r/lyt/lyt_layout.h>
+
+#include <decomp.h>
 
 class CMCCylinderGauge {
 public:
@@ -19,7 +19,7 @@ public:
     bool func_802220F0();
     void func_802220F8();
     void func_80222118();
-    void func_802221A4(f32);
+    void func_802221A4(float);
     void func_80222234();
     u8 func_80222258();
     void func_802222A4(u8);
@@ -32,15 +32,15 @@ public:
     DECOMP_DONT_INLINE void func_80222630();
     DECOMP_DONT_INLINE void func_802226B8();
 
-    nw4r::lyt::ArcResourceAccessor* pArcResourceAccessor;   // 0x04
-    nw4r::lyt::Layout* mLayout;                 // 0x08
-    nw4r::lyt::AnimTransform* pAnimTrans_0x0C;  // 0x0C
-    nw4r::lyt::AnimTransform* pAnimTrans_0x10;  // 0x10
-    nw4r::lyt::AnimTransform* pAnimTrans_0x14;  // 0x14
-    nw4r::lyt::AnimTransform* pAnimTrans_0x18;  // 0x18
-    bool bool_0x1c;                             // 0x1c
-    u32 unk_0x20;                               // 0x20
-    bool bool_0x24;                             // 0x24
-    u8 unk_0x25;                                // 0x25
-    f32 float_0x28;                             // 0x28
+    nw4r::lyt::ArcResourceAccessor* mArcResourceAccessor; //0x4
+    nw4r::lyt::Layout* mLayout;                           //0x8
+    nw4r::lyt::AnimTransform* mAnimTransC;
+    nw4r::lyt::AnimTransform* mAnimTrans10;
+    nw4r::lyt::AnimTransform* mAnimTrans14;
+    nw4r::lyt::AnimTransform* mAnimTrans18;
+    bool unk1c;
+    u32 unk20;
+    bool unk24;
+    u8 unk25;
+    float unk28;
 };
