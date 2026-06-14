fe9e5350662627dba45ddd32dc28823563442c31
Noaseto <noix.50@hotmail.fr>
2026-06-14 13:47:34 +0200

    CMCeff{Red,Blue,Green} mostly done

--- src/kyoshin/makecrystal/CMCEffStart.cpp
+++ src/kyoshin/makecrystal/CMCEffStart.cpp
@@ -3,6 +3,7 @@
 #include "kyoshin/code_80135FDC.hpp"
 
 extern u32 func_80137444(nw4r::lyt::AnimTransform*, float);
+extern void func_80138078(u32);
 
 CMCEffStart::CMCEffStart(nw4r::lyt::ArcResourceAccessor* arcResourceAccessor)
     : unk4(0), unk5(1), mArcResourceAccessor(arcResourceAccessor), mLayout(nullptr), mAnimTrans(nullptr), unk14(0) {}
@@ -58,4 +59,73 @@ void CMCEffStart::func_80223F44() {
         unk4 = 0;
         unk5 = 1;
     }
+}
+/******************************************************************************
+ *
+ * CMCEffUpRed
+ *
+ ******************************************************************************/
+CMCEffUpRed::CMCEffUpRed(nw4r::lyt::ArcResourceAccessor* pArcResourceAccessor) : CMCEffStart(pArcResourceAccessor) {}
+
+CMCEffUpRed::~CMCEffUpRed() {}
+
+void CMCEffUpRed::func_8022400C() {
+    char* string = "mf10_cry02_prt00";
+    func_80136E84(&mLayout, mArcResourceAccessor, string);
+    char* string2 = "mf10_cry02_prt00_in";
+    func_80136F08(mLayout, &mAnimTrans, mArcResourceAccessor, string2);
+    mLayout->SetAnimationEnable(mAnimTrans, true);
+    mLayout->Animate(false);
+    unk14 = 1;
+}
+
+void CMCEffUpRed::func_8022409C() {
+    func_80223F24();
+    func_80138078(0x8c); //140 Dec
+}
+/******************************************************************************
+ *
+ * CMCEffUpBlue
+ *
+ ******************************************************************************/
+CMCEffUpBlue::CMCEffUpBlue(nw4r::lyt::ArcResourceAccessor* pArcResourceAccessor) : CMCEffStart(pArcResourceAccessor) {}
+
+CMCEffUpBlue::~CMCEffUpBlue() {}
+
+void CMCEffUpBlue::func_80224140() {
+    char* string = "mf10_cry02_prt01";
+    func_80136E84(&mLayout, mArcResourceAccessor, string);
+    char* string2 = "mf10_cry02_prt01_in";
+    func_80136F08(mLayout, &mAnimTrans, mArcResourceAccessor, string2);
+    mLayout->SetAnimationEnable(mAnimTrans, true);
+    mLayout->Animate(false);
+    unk14 = 1;
+}
+
+void CMCEffUpBlue::func_802241D0() {
+    func_80223F24();
+    func_80138078(0x8c); //140 Dec
+}
+/******************************************************************************
+ *
+ * CMCEffUpGreen
+ *
+ ******************************************************************************/
+CMCEffUpGreen::CMCEffUpGreen(nw4r::lyt::ArcResourceAccessor* pArcResourceAccessor) : CMCEffStart(pArcResourceAccessor) {}
+
+CMCEffUpGreen::~CMCEffUpGreen() {}
+
+void CMCEffUpGreen::func_80224274() {
+    char* string = "mf10_cry02_prt02";
+    func_80136E84(&mLayout, mArcResourceAccessor, string);
+    char* string2 = "mf10_cry02_prt02_in";
+    func_80136F08(mLayout, &mAnimTrans, mArcResourceAccessor, string2);
+    mLayout->SetAnimationEnable(mAnimTrans, true);
+    mLayout->Animate(false);
+    unk14 = 1;
+}
+
+void CMCEffUpGreen::func_80224304() {
+    func_80223F24();
+    func_80138078(0x8c); //140 Dec
 }
--- src/kyoshin/makecrystal/CMCEffStart.hpp
+++ src/kyoshin/makecrystal/CMCEffStart.hpp
@@ -7,7 +7,11 @@
 #include <nw4r/lyt/lyt_layout.h>
 
 #include <decomp.h>
-
+/******************************************************************************
+ *
+ * CMCEffStart
+ *
+ ******************************************************************************/
 class CMCEffStart {
 public:
     CMCEffStart(nw4r::lyt::ArcResourceAccessor*);
@@ -17,7 +21,7 @@ public:
     void func_80223E9C(nw4r::lyt::DrawInfo*);
     void func_80223EBC();
     u8 func_80223F1C();
-    void func_80223F24();
+    DECOMP_DONT_INLINE void func_80223F24();
     DECOMP_DONT_INLINE void func_80223F44();
 
     u8 unk4;
@@ -26,4 +30,40 @@ public:
     nw4r::lyt::Layout* mLayout;                           //0xc
     nw4r::lyt::AnimTransform* mAnimTrans;                 //0x10
     u8 unk14;
+};
+/******************************************************************************
+ *
+ * CMCEffUpRed
+ *
+ ******************************************************************************/
+class CMCEffUpRed : CMCEffStart {
+public:
+    CMCEffUpRed(nw4r::lyt::ArcResourceAccessor*);
+    virtual ~CMCEffUpRed();
+    void func_8022400C();
+    void func_8022409C();
+};
+/******************************************************************************
+ *
+ * CMCEffUpBlue
+ *
+ ******************************************************************************/
+class CMCEffUpBlue : CMCEffStart {
+public:
+    CMCEffUpBlue(nw4r::lyt::ArcResourceAccessor*);
+    virtual ~CMCEffUpBlue();
+    void func_80224140();
+    void func_802241D0();
+};
+/******************************************************************************
+ *
+ * CMCEffUpGreen
+ *
+ ******************************************************************************/
+class CMCEffUpGreen : CMCEffStart {
+public:
+    CMCEffUpGreen(nw4r::lyt::ArcResourceAccessor*);
+    virtual ~CMCEffUpGreen();
+    void func_80224274();
+    void func_80224304();
 };
